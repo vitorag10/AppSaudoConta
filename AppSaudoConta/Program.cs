59aa769d92a1781c995920f891309dc74e1e3da3
vitor <vitor.hag@outlook.com>
2023-06-06 17:49:21 -0300

    Adicionar arquivos de projeto.

---
+++ AppSaudoConta/Program.cs
@@ -0,0 +1,17 @@
+﻿double saldo, debito, credito, SaldoAtual;
+string conta;
+Console.WriteLine("Insira o número da conta do cliente:");
+conta = Console.ReadLine();
+Console.WriteLine("Insira o saldo do cliente:");
+saldo = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Insira o dépito do cliente:");
+debito = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Insira o crédito do cliente:");
+credito = Convert.ToDouble(Console.ReadLine());
+SaldoAtual = saldo - debito + credito;
+Console.WriteLine("\n\tO saldo atual é R${0}",
+SaldoAtual);
+if (SaldoAtual > 0)
+{ Console.WriteLine("Saldo Positivo"); }
+else
+{ Console.WriteLine("Saldo Negativo"); }
